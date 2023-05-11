75f2efcf97362d14e557c555c96be3e9117bcadd
Charizard6 <yyh6066@naver.com>
2023-05-11 16:32:57 +0900

    week 11

--- src/index.js
+++ src/index.js
@@ -10,15 +10,16 @@ import reportWebVitals from './reportWebVitals';
 // import Accommodate from './chapter_07/Accommodate';
 // import UseRef from './chapter_07/UseRef';
 // import ConfirmButton from './chapter_08/ConfirmButton';
-import LandingPage from './chapter_09/LandingPage';
-import AttendanceBook from './chapter_10/AttendanceBook';
-import SignUp from './chapter_11/SignUp';
+// import LandingPage from './chapter_09/LandingPage';
+// import AttendanceBook from './chapter_10/AttendanceBook';
+// import SignUp from './chapter_11/SignUp';
+import Calculator from './chapter_12/Calculator';
 //import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SignUp/>
+    <Calculator />
   </React.StrictMode>
 )
 
